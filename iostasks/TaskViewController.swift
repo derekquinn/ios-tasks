b5f2e9707c3e3480b93cb0fa965cdef0a416e33b
derek quinn <dereq@Troy.local>
2020-07-20 18:53:06 -0400

    Implemented swipe to delete

--- iostasks/TaskViewController.swift
+++ iostasks/TaskViewController.swift
@@ -2,11 +2,20 @@
 import UIKit
 
 class TaskViewController: UIViewController {
-
+    
+    @IBOutlet var label: UILabel!
+    var task: String?
+    var currentPosition: Int?
+    var count: Int?
+    var update: (() -> Void)?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        label.text = task
+        
     }
     
-
 }
+
+
--- iostasks/ViewController.swift
+++ iostasks/ViewController.swift
@@ -3,24 +3,46 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
+    
     @IBOutlet var tableView: UITableView!
+    
     var tasks = [String]()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        updateTasks()
         // Extract to constant
         self.title = "Tasks"
         tableView.delegate = self
         tableView.dataSource = self 
         // Get all currently saved tasks
-        
         // Saving setup
         
-        if !UserDefaults().bool(forKey: "setup"){
-            UserDefaults().set(true, forKey: "setup")
-            UserDefaults().set(0, forKey: "count") // Number of tasks user currently has
+        if !UserDefaults.standard.bool(forKey:"setup"){
+            
+            UserDefaults.standard.set(true, forKey:"setup")
+            UserDefaults.standard.set(0, forKey: "count")
+        }
+        
+        
+    }
+    // swipe to delete
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        
+        if editingStyle == .delete{
+            
+            var newCount = tasks.count
+            newCount = newCount-1
+            
+            let index = indexPath.row
+            
+            tasks.remove(at: indexPath.row)
+            
+            self.tableView.deleteRows(at: [indexPath], with: .fade)
+            
+            UserDefaults.standard.set(newCount, forKey: "count")
+            UserDefaults.standard.removeObject(forKey: "task_\(index)")
         }
         
         
@@ -28,16 +50,16 @@ class ViewController: UIViewController {
     }
     
     func updateTasks(){
-        
+        print("updateTasks()")
         tasks.removeAll()
-        guard let count = UserDefaults().value(forKey: "count") as? Int else {
+        
+        guard let count = UserDefaults.standard.value(forKey: "count") as? Int else {
             return
         }
-        
         // count is inclusive
         for x in 0..<count {
-             
-            if let task = UserDefaults().value(forKey: "task_\(x+1)") as? String  {
+            
+            if let task = UserDefaults.standard.value(forKey: "task_\(x+1)") as? String  { // make sure it's a string
                 tasks.append(task)
             }
         }
@@ -52,21 +74,29 @@ class ViewController: UIViewController {
         //reload the table view when add tasks
         vc.update = {
             DispatchQueue.main.async { // prioritize updating the tasks
-                      self.updateTasks()
+                self.updateTasks()
             }
         }
         
         navigationController?.pushViewController(vc, animated: true)
     }
-
+    
 }
 
 extension ViewController: UITableViewDelegate {
     
     func tableView (_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
         tableView.deselectRow(at: indexPath, animated: true)
+        
+        let vc = storyboard?.instantiateViewController(withIdentifier: "task") as! TaskViewController
+        // Extract to constant
+        vc.title = "Update / Delete Task"
+        vc.task = tasks[indexPath.row]
+        vc.currentPosition = indexPath.row
+        vc.count = tasks.count
+        //print ("Selected \(vc.task)")
+        navigationController?.pushViewController(vc, animated: true)
     }
-    
 }
 
 extension ViewController: UITableViewDataSource {
